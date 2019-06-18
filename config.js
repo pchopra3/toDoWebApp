7e71941b099124019b2915eba3e8ab0cbe6e10a2
pchopra3 <51158877+pchopra3@users.noreply.github.com>
2019-06-18 13:01:57 +0530

    Update config.js
    Removing the api key and other details of the firebase project

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 import firebase from 'firebase';
 
 export const DB_CONFIG = {
-    apiKey: "AIzaSyBrVnrsuX9gkZwsEamIFE5pWP7hYU8Yt4I",
-    authDomain: "my-new-project-c47ec.firebaseapp.com",
-    databaseURL: "https://my-new-project-c47ec.firebaseio.com",
-    projectId: "my-new-project-c47ec",
-    storageBucket: "my-new-project-c47ec.appspot.com",
-    messagingSenderId: "948509943776",
-    appId: "1:948509943776:web:657c6c655153af98"
+   
 };
